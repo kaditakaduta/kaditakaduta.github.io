8e31ca1649e0289a6bc943cdc62ab7eed06d4268
kaditakaduta <apple_ba@163.com>
2024-11-10 16:37:47 +0800

    update

---
+++ up.py
@@ -0,0 +1,15 @@
+import codecs
+inputf = codecs.open("README.md",'r',encoding='utf-8')
+page = 1
+outputf = codecs.open("page" + str(page) + ".md", "w", encoding='utf-8')
+linenum = 0
+for line in inputf:
+    linenum += 1
+    outputf.write(line)
+    if linenum % 20 == 0:
+        page += 1
+        outputf.write("\r\n[next](page" + str(page) + ".md)")
+        outputf.close()
+        outputf = codecs.open("page" + str(page) + ".md", "w", encoding='utf-8')
+        linenum = 0
+outputf.close()
